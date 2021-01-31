df0d9074f101ee392930c7e6602a1f8d423d7604
peeyush jain <95peeyushjain@gmail.com>
2021-02-01 00:13:23 +0530

    wait() & notify()

--- src/multithreading/guardedblock/GuardedBlock.java
+++ src/multithreading/guardedblock/GuardedBlock.java
@@ -1,13 +1,55 @@
 package multithreading.guardedblock;
 
 public class GuardedBlock {
-    static class Greeting{
-        boolean greetingRecieved = false;
-        public void isHappy(){
-            while(!greetingRecieved);
-            /*
-            * example of guarded block as this code will be executed only when greetingRecieved is true*/
+    static class Greeting {
+       volatile boolean greetingRecieved = false;
+
+/*        public synchronized void isHappy()  {
+            while (!greetingRecieved) {
+                System.out.println("waiting for greeting");
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException interruptedException) {
+//                    interruptedException.printStackTrace();
+//                }
+            }
+            *//*
+             * example of guarded block as this code will be executed only when greetingRecieved is true*//*
             System.out.println("greeting received happy now");
+        }*/
+
+
+        public synchronized void isHappy() {
+            while (!greetingRecieved) {
+                try {
+                    System.out.println("waiting for greeting");
+                    wait();
+                } catch (InterruptedException exception) {
+                    System.out.println(exception);
+                }
+            }
+            System.out.println("greeting received happy now");
+        }
+
+        public synchronized void sendNotification() {
+            System.out.println("Sending Greeting");
+            this.greetingRecieved = true;
+            System.out.println("Greeting sent");
+            notifyAll();
+        }
+
+        public static void main(String[] args) {
+            Greeting greeting = new Greeting();
+            new Thread(greeting::isHappy).start();
+            new Thread(() -> {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+                greeting.sendNotification();
+            }).start();
+
         }
     }
 }
